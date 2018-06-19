19f5117105faf0b6ae2d5f99a1703a9411f82cb5
Sudhanshu Siddh <ssiddh@iu.edu>
2018-06-19 19:36:45 -0400

    Added logic to handle orientation changes on characterDetail

--- lib/pages/character_detail.dart
+++ lib/pages/character_detail.dart
@@ -21,7 +21,7 @@ class CharacterDetail extends StatelessWidget {
         margin: EdgeInsets.all(8.0),
         color:
             _character.status == "Alive" ? Colors.green[700] : Colors.red[300],
-        child: new Column(
+        child: MediaQuery.of(context).orientation == Orientation.portrait ? new Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             _createImageAvatar(_character.image),
@@ -31,7 +31,15 @@ class CharacterDetail extends StatelessWidget {
             _createListTile(Icons.home, _character.originName, text_theme),
             _createListTile(Icons.my_location, _character.lastLocation, text_theme),
           ],
-        ),
+        ) : new Row(mainAxisSize: MainAxisSize.min, children: <Widget>[_createImageAvatar(_character.image), new Expanded(flex: 1, child: new Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: new Column(children: <Widget>[
+          _createListTile(Icons.person, _character.name, text_theme),
+          _createListTile(Icons.person_pin, _character.species, text_theme),
+          _createListTile(Icons.face, _character.gender, text_theme),
+          _createListTile(Icons.home, _character.originName, text_theme),
+          _createListTile(Icons.my_location, _character.lastLocation, text_theme),],),
+        ),)],),
       ),
     );
   }
@@ -51,7 +59,7 @@ class CharacterDetail extends StatelessWidget {
 
   Widget _createImageAvatar(String imageUrl) {
     return new Container(
-      margin: EdgeInsets.only(top: 16.0),
+      margin: EdgeInsets.all(16.0),
       width: 200.0,
       height: 200.0,
       decoration: new BoxDecoration(
@@ -60,5 +68,9 @@ class CharacterDetail extends StatelessWidget {
             image: new NetworkImage(imageUrl), fit: BoxFit.cover),
       ),
     );
+  }
+
+  Widget _generatePortraitScreen() {
+
   }
 }
