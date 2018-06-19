0b8abd88ab66800d5ba1a71f747941ecbaa0dd0c
Sudhanshu Siddh <ssiddh@iu.edu>
2018-06-19 16:10:01 -0400

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('RickOPedia'),
+          backgroundColor: Colors.green[400],
+        ),
+        body: new Center(
+          child: new Container(
+            child: new Text(
+              'Sample App to learn Flutter',
+              style: Theme.of(context).textTheme.headline,
+            ),
+            color: Colors.white70,
+          ),
+        ),
+      ),
+    );
+  }
+}
