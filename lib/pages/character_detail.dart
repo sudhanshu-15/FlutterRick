f6493e5cef561b3f9c8c67e75493f0dcbab2b2a3
Sudhanshu Siddh <ssiddh@iu.edu>
2018-06-19 19:24:25 -0400

    Refined the details UI, code for orientation required

--- lib/pages/character_detail.dart
+++ lib/pages/character_detail.dart
@@ -6,39 +6,58 @@ class CharacterDetail extends StatelessWidget {
 
   CharacterDetail(this._character);
 
+  static const icon_size = 40.0;
+
   @override
   Widget build(BuildContext context) {
+    var text_theme =
+        Theme.of(context).textTheme.title.copyWith(color: Colors.white);
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(_character.name),
       ),
       body: new Card(
         margin: EdgeInsets.all(8.0),
-        color: Colors.green[700],
+        color:
+            _character.status == "Alive" ? Colors.green[700] : Colors.red[300],
         child: new Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            new Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: new GridTile(child: new Image.network(_character.image)),
-            ),
-            new ListTile(
-              leading: new Icon(
-                Icons.person,
-                size: 40.0,
-                color: Colors.white,
-              ),
-              title: new Text(
-                _character.name,
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .title
-                    .copyWith(color: Colors.white),
-              ),
-            )
+            _createImageAvatar(_character.image),
+            _createListTile(Icons.person, _character.name, text_theme),
+            _createListTile(Icons.person_pin, _character.species, text_theme),
+            _createListTile(Icons.face, _character.gender, text_theme),
+            _createListTile(Icons.home, _character.originName, text_theme),
+            _createListTile(Icons.my_location, _character.lastLocation, text_theme),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _createListTile(IconData icon, String text, TextStyle textTheme) {
+    return new ListTile(
+        leading: new Icon(
+          icon,
+          size: icon_size,
+          color: Colors.white,
+        ),
+        title: new Text(
+          text,
+          style: textTheme,
+        ));
+  }
+
+  Widget _createImageAvatar(String imageUrl) {
+    return new Container(
+      margin: EdgeInsets.only(top: 16.0),
+      width: 200.0,
+      height: 200.0,
+      decoration: new BoxDecoration(
+        shape: BoxShape.circle,
+        image: new DecorationImage(
+            image: new NetworkImage(imageUrl), fit: BoxFit.cover),
       ),
     );
   }
