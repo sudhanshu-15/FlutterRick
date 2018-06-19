d21bc257a0178db8caf07550207e6cef93b0ccc3
Sudhanshu Siddh <ssiddh@iu.edu>
2018-06-19 16:20:12 -0400

    Added boiler plate code for NetworkUtils

---
+++ lib/api/api.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NetworkUtil {
+
+  static NetworkUtil _instance = new NetworkUtil.internal();
+  NetworkUtil.internal();
+  factory NetworkUtil() => _instance;
+
+  final JsonDecoder _decoder = new JsonDecoder();
+
+  Future<dynamic> get(String url) {
+    return http.get(url).then((http.Response response) {
+      final String responseBody = response.body;
+      final int statusCode = response.statusCode;
+
+      if (statusCode < 200 || statusCode > 400 || json == null) {
+        throw new Exception("Error while fetching data");
+      }
+      return _decoder.convert(responseBody);
+    });
+  }
+}
