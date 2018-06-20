14be45991e817b33e8c3ff074090c9c89ffbbebe
Sudhanshu Siddh <ssiddh@iu.edu>
2018-06-20 10:28:42 -0400

    Added gradient to the cards

--- lib/pages/character_detail.dart
+++ lib/pages/character_detail.dart
@@ -14,44 +14,88 @@ class CharacterDetail extends StatelessWidget {
         Theme.of(context).textTheme.title.copyWith(color: Colors.white);
 
     Widget _generatePortraitScreen() {
-      return new Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          _createImageAvatar(_character.id, _character.image),
-          _createListTile(Icons.person, _character.name, text_theme),
-          _createListTile(Icons.person_pin, _character.species, text_theme),
-          _createListTile(Icons.face, _character.gender, text_theme),
-          _createListTile(Icons.home, _character.originName, text_theme),
-          _createListTile(
-              Icons.my_location, _character.lastLocation, text_theme),
-        ],
+      return new Container(
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+            stops: [0.1, 0.5, 0.7, 0.9],
+            colors: _character.status == "Alive"
+                ? [
+                    Colors.green[600],
+                    Colors.green[500],
+                    Colors.blue[400],
+                    Colors.blue[200]
+                  ]
+                : [
+                    Colors.red[600],
+                    Colors.red[400],
+                    Colors.deepOrange[400],
+                    Colors.deepOrange[200],
+                  ],
+          ),
+        ),
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _createImageAvatar(_character.id, _character.image),
+            _createListTile(Icons.person, _character.name, text_theme),
+            _createListTile(Icons.person_pin, _character.species, text_theme),
+            _createListTile(Icons.face, _character.gender, text_theme),
+            _createListTile(Icons.home, _character.originName, text_theme),
+            _createListTile(
+                Icons.my_location, _character.lastLocation, text_theme),
+          ],
+        ),
       );
     }
 
     Widget _generateLandscapeScreen() {
-      return new Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          _createImageAvatar(_character.id, _character.image),
-          new Expanded(
-            flex: 1,
-            child: new Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: new Column(
-                children: <Widget>[
-                  _createListTile(Icons.person, _character.name, text_theme),
-                  _createListTile(
-                      Icons.person_pin, _character.species, text_theme),
-                  _createListTile(Icons.face, _character.gender, text_theme),
-                  _createListTile(
-                      Icons.home, _character.originName, text_theme),
-                  _createListTile(
-                      Icons.my_location, _character.lastLocation, text_theme),
-                ],
+      return new Container(
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+            stops: [0.1, 0.5, 0.7, 0.9],
+            colors: _character.status == "Alive"
+                ? [
+                    Colors.green[600],
+                    Colors.green[500],
+                    Colors.blue[400],
+                    Colors.blue[200]
+                  ]
+                : [
+                    Colors.red[600],
+                    Colors.red[400],
+                    Colors.deepOrange[400],
+                    Colors.deepOrange[200],
+                  ],
+          ),
+        ),
+        child: new Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _createImageAvatar(_character.id, _character.image),
+            new Expanded(
+              flex: 1,
+              child: new Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Column(
+                  children: <Widget>[
+                    _createListTile(Icons.person, _character.name, text_theme),
+                    _createListTile(
+                        Icons.person_pin, _character.species, text_theme),
+                    _createListTile(Icons.face, _character.gender, text_theme),
+                    _createListTile(
+                        Icons.home, _character.originName, text_theme),
+                    _createListTile(
+                        Icons.my_location, _character.lastLocation, text_theme),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       );
     }
 
@@ -61,9 +105,6 @@ class CharacterDetail extends StatelessWidget {
       ),
       body: new Card(
           margin: EdgeInsets.all(8.0),
-          color: _character.status == "Alive"
-              ? Colors.green[700]
-              : Colors.red[300],
           child: MediaQuery.of(context).orientation == Orientation.portrait
               ? _generatePortraitScreen()
               : _generateLandscapeScreen()),
