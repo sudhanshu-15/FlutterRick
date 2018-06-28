66753c4a9fc95f34b86cc9decc72086211fe1425
Sudhanshu Siddh <ssiddh@iu.edu>
2018-06-28 00:41:23 -0400

    Created a bottom nav bar with notch for fab

--- lib/pages/character_list.dart
+++ lib/pages/character_list.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rick/api/repository.dart';
-import 'package:flutter_rick/models/character.dart';
-import 'package:flutter_rick/pages/character_detail.dart';
-import 'package:flutter_rick/pages/character_list_bloc_screen.dart';
 import 'package:flutter_rick/widgets/character_grid.dart';
 import 'package:http/http.dart' as http;
 
@@ -27,17 +24,6 @@ class CharacterList extends StatelessWidget {
           List characters = snapshot.data;
           return CharacterGrid(characters);
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.swap_calls,
-          size: 30.0,
-        ),
-        onPressed: () => Navigator.of(context).push(
-              new MaterialPageRoute(
-                builder: (BuildContext context) => CharacterListBlocScreen(),
-              ),
-            ),
       ),
     );
   }
